be76bae094284fb09573fe9d1b6a8ec0f636b9ad
jtaeyeon05 <jtaeyeon05@gmail.com>
2026-04-26 18:38:43 +0900

    feat: Initialize project with Vite and React
    - Set up Vite for a modern frontend build process.
    - Integrated React and ReactDOM for component-based UI development.
    - Configured ESLint and TypeScript for code quality and maintainability.
    - Organized project structure and added essential configuration files.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p><a href="https://kmp-mnist.xodus.lol/">kmp-mnist</a></p>
+      <p><a href="https://why.xodus.lol/">why-web</a></p>
+      <p><a href="https://xodus.lol/">xodus.lol</a></p>
+      <p><a href="/SpotlightPlayer/index.html">SpotlightPlayer</a></p>
+      <p><a href="/flutter2048/index.html">flutter2048</a></p>
+      <p><a href="/flutterLearn/index.html">flutterLearn</a></p>
+      <p><a href="/getCOOL/index.html">getCOOL</a></p>
+      <footer>
+        <p>jtaeyeon05.github.io</p>
+        <p>&copy; jtaeyeon05 | 2026 | email@xodus.lol</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
